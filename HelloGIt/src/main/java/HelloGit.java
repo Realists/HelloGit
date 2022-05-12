7b132d9196eb35d675351dd44c93dc1878d5cd61
Realists <1192956208@qq.com>
2022-05-12 17:41:37 +0800

    分支添加

--- HelloGIt/src/main/java/HelloGit.java
+++ HelloGIt/src/main/java/HelloGit.java
@@ -1,8 +1,11 @@
+import org.junit.Test;
+
 /**
  * @author 绘世浮城
  * @create 2022-05-12 9:35
  */
 public class HelloGit {
+    @Test
     public static void main(String[] args) {
         System.out.println("Hello Git！");
         System.out.println("版本提交");
